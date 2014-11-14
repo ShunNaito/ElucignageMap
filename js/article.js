c3dbb9d3c4029f28d4f0fe043735393a0ee0462f
Shun <s.n.systems.engineer@gmail.com>
2014-11-14 15:48:33 +0900

    #5 インデントを直した

--- js/article.js
+++ js/article.js
@@ -1,29 +1,21 @@
 var parseDate = d3.time.format("%Y-%m-%d").parse;
 
-		function displayText(){
-			// データを読み込む
-		    d3.csv("./EbolaArticle.csv", function(error, data) {
-		        // データをフォーマット
-		        data.forEach(function(d) {
-		            d.date = parseDate(d.date);
-		        });
+function displayText(){
+	// データを読み込む
+    d3.csv("./EbolaArticle.csv", function(error, data) {
+        // データをフォーマット
+        data.forEach(function(d) {
+            d.date = parseDate(d.date);
+        });
 
-			    //　新聞記事の本文を表示する
-			    d3.select("#articlePane").selectAll("li")
-			    	.data(data)
-     				.enter()
-     				.append("li")
-     				.attr("id",function(d) {
- 						return d.date;
-     				})
-     				// .attr("class", "article")
-     				.text(function(d) { return d.text});
-				// for(var i=0; i<=data.length-1; i++){
-				// 	var $div = $('<div>')
-		  //       		.attr({"id": d[i].date,
-		  //       			"class": 'article'});
-			 //        $div.append(d[i].text+'<br>');
-			 //        $('#articlePane').append($div);
-			 //    }
-			});
-		}
+		//　新聞記事の本文を表示する
+		d3.select("#articlePane").selectAll("li")
+			.data(data)
+			.enter()
+			.append("li")
+			.attr("id",function(d) {
+				return d.date;
+			})
+			.text(function(d) { return d.text});
+	});
+}
