b9043503684fbf5f09b7c4d68bb6f72738361f43
Shun <s.n.systems.engineer@gmail.com>
2014-11-14 15:44:51 +0900

    #9 GuineaをクリックするとGuineaのエボラ感染者数のグラフに切り替わるようにした

--- js/graph.js
+++ js/graph.js
@@ -22,48 +22,56 @@ var yAxis = d3.svg.axis()
 // 線の定義
 var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.TheTotalNumberOfPatients); });
+    .y(function(d) { return y(d.close); });
 
 // svgの定義
 var svg = d3.select("#graph").append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .attr("height", height + margin.top + margin.bottom);
+
+var focus = svg.append("g")
+    .attr("class", "focus")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// データを読み込む
-d3.csv("ebora.csv", function(error, data) {
-  // データをフォーマット
-  data.forEach(function(d) {
-    d.date = parseDate(d.date);
-    d.TheTotalNumberOfPatients = +d.TheTotalNumberOfPatients;
+drawGraph("Total");
+
+function drawGraph(statisticsName){
+  $('.focus').empty();
+
+  // データを読み込む
+  d3.csv("Ebola"+statisticsName+".csv", function(error, data) {
+    // データをフォーマット
+    data.forEach(function(d) {
+      d.date = parseDate(d.date);
+      d.close = +d.close;
+    });
+
+    // データを入力ドメインとして設定
+    // 同時にextentで目盛りの単位が適切になるようにする
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain(d3.extent(data, function(d) { return d.close; }));
+
+    // x軸をsvgに表示
+    focus.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // y軸をsvgに表示
+    focus.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Casualties (人)");
+
+    // path要素をsvgに表示し、折れ線グラフを設定
+    focus.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
   });
-
-  // データを入力ドメインとして設定
-  // 同時にextentで目盛りの単位が適切になるようにする
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.TheTotalNumberOfPatients; }));
-
-  // x軸をsvgに表示
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  // y軸をsvgに表示
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Casualties (人)");
-
-  // path要素をsvgに表示し、折れ線グラフを設定
-  svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", line);
-});
+}
