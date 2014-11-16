4c7bb7dc5d7d06287661a459f80225b047752350
Shun <s.n.systems.engineer@gmail.com>
2014-11-16 11:53:03 +0900

    #11 csvデータから国名を取得した
    csvデータから国名を取得し、地図にマッピングした

--- js/timeslider.js
+++ js/timeslider.js
@@ -1,5 +1,6 @@
 // タイムスライダー
-d3.csv("data/EbolaArticle.csv", function(error, data) {
+d3.csv("data/EbolaCaces.csv", function(error, data) {
+    var countryNameArray = Object.keys(data[0]);
     // 時系列情報をパース
     data.forEach(function(d) {
         d.date = parseDate(d.date);
@@ -11,16 +12,24 @@ d3.csv("data/EbolaArticle.csv", function(error, data) {
     var max = d3.max(data, function(d) { return d.date; }).getDate();
     //　タイムスライダーのインタラクション
     d3.select('#slider').call(d3.slider().axis(true).min(min).max(max).step(1).on("slide", function(evt, value) {
-	// テスト（スライダーの値をコンソールに表示）
-	console.log(value);
-	//　値（時系列）に応じて記事をハイライト
-	for(var i=0; i<=data.length-1; i++){
-	    if(value == data[i].date.getDate()){
-		document.getElementById(data[i].date).style.color = "red";
-		$('.datamaps-subunit.AGO').css('fill','#C99');
-	    }else{
-		document.getElementById(data[i].date).style.color = "black";
-	    }
-	}
+        var countryName = countryNameArray[1];
+
+        $('.datamaps-subunit'+"."+countryName).css('fill','#C99');
+        // テスト（スライダーの値をコンソールに表示）
+        console.log(value);
+        d3.csv("data/EbolaArticle.csv", function(error, data) {
+            // 時系列情報をパース
+            data.forEach(function(d) {
+                d.date = parseDate(d.date);
+            });
+            //　値（時系列）に応じて記事をハイライト
+            for(var i=0; i<=data.length-1; i++){
+                if(value == data[i].date.getDate()){
+                    document.getElementById(data[i].date).style.color = "red";
+                }else{
+                    document.getElementById(data[i].date).style.color = "black";
+                }
+            }
+        });
     }));
 });
