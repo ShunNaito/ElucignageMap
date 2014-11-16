d19709162f74779026e561fd973618252d33df65
Shun <s.n.systems.engineer@gmail.com>
2014-11-16 20:25:21 +0900

    #11　タイムスライダーの日付に応じた統計情報のマッピング
    タイムスライダーの日付に応じてギニアのエボラ感染者数を地図にマッピングした

--- js/timeslider.js
+++ js/timeslider.js
@@ -1,6 +1,5 @@
 // タイムスライダー
-d3.csv("data/EbolaCaces.csv", function(error, data) {
-    var countryNameArray = Object.keys(data[0]);
+d3.csv("data/EbolaGuinea.csv", function(error, data) {
     // 時系列情報をパース
     data.forEach(function(d) {
         d.date = parseDate(d.date);
@@ -10,13 +9,21 @@ d3.csv("data/EbolaCaces.csv", function(error, data) {
     var min = d3.min(data, function(d) { return d.date; }).getDate();
     //　目盛りの最大値
     var max = d3.max(data, function(d) { return d.date; }).getDate();
+
     //　タイムスライダーのインタラクション
     d3.select('#slider').call(d3.slider().axis(true).min(min).max(max).step(1).on("slide", function(evt, value) {
-        var countryName = countryNameArray[1];
-
-        $('.datamaps-subunit'+"."+countryName).css('fill','#C99');
         // テスト（スライダーの値をコンソールに表示）
         console.log(value);
+        for(var i=0; i<=data.length-1; i++){
+            if(value == data[i].date.getDate()){
+                if(data[i].close >= 1500){
+                    $('.datamaps-subunit.GIN').css('fill','#C10');
+                }else{
+                    $('.datamaps-subunit.GIN').css('fill','#C99');
+                }
+            }
+        }
+
         d3.csv("data/EbolaArticle.csv", function(error, data) {
             // 時系列情報をパース
             data.forEach(function(d) {
