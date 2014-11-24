7f595f29780c4450aa72c862ab3d975ca5a50527
Shun <s.n.systems.engineer@gmail.com>
2014-11-24 15:07:13 +0900

    アメリカの地図を変更

--- js/usaMap.js
+++ js/usaMap.js
@@ -1,11 +1,16 @@
-var election = new Datamap({
-  scope: 'usa',
-  element: document.getElementById('map_election'),
-  geographyConfig: {
-    highlightBorderColor: '#bada55',
-    highlightBorderWidth: 3
-  },
+var zoom2 = new Datamap({
+  element: document.getElementById("usa"),
+  scope: 'world',
+  // Zoom in on Africa
+  setProjection: function(element) {
+    var projection = d3.geo.equirectangular()
+      .center([-110, 40])
+      .rotate([4.4, 0])
+      .scale(200)
+      .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+    var path = d3.geo.path()
+      .projection(projection);
 
+    return {path: path, projection: projection};
   }
 });
-election.labels();
