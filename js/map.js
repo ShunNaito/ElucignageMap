366631bf96df61bd69e01b749cfd719ad33cb89e
otknoy <otknoy@gmail.com>
2014-12-12 13:40:21 +0900

    Fix syntax error

--- js/map.js
+++ js/map.js
@@ -38,7 +38,7 @@ $(function(){
 		// 1行目が日本語なので安全のためラベル名など割り当て直す
 		//GIN,LBR,SLE,NGA,SEN,USA
 		// 将来的には国の省略語と名前（英語や日本語）が全て対応づくイメージ
-		return {date : d.date, close : d["close"],GIN : d["ギニア"], LBR : d["リベリア"], SLE : d["シエラレオネ"], NGA : d["ナイジェリア"], SEN : d["セネガル"], USA : d["アメリカ"], MLI : d["マリ"], ESP : d["スペイン"] }
+		return {date : d.date, close : d["close"],GIN : d["ギニア"], LBR : d["リベリア"], SLE : d["シエラレオネ"], NGA : d["ナイジェリア"], SEN : d["セネガル"], USA : d["アメリカ"], MLI : d["マリ"], ESP : d["スペイン"] };
 	})
 	.get(function(error, data) {
 		var countryNameArray = Object.keys(data[0]);
