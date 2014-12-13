4c776f6f8f86e8a763981834bcf11298315df6e6
Shun <s.n.systems.engineer@gmail.com>
2014-12-13 14:55:34 +0900

    Revert "Add click event to data maps"
    This reverts commit c5e4a79d3048641130112b1d443c580479170e05, reversing
    changes made to 308767db2540bef174fa3340ecadcdbdcdf511d3.

--- js/map.js
+++ js/map.js
@@ -26,6 +26,7 @@ function createDatamap(id, type) {
 	setProjection: setProjection
     });
     return map;
+<<<<<<< HEAD
 }
 
 var afcicaMap = createDatamap("africa", "africa");
@@ -50,3 +51,11 @@ $(function(){
 		}
 	});
 });
+=======
+};
+
+map.create = function(stats) {
+    var afcicaMap = map.createDatamap("africa", "africa");
+    var usaMap = map.createDatamap("usa", "usa");
+};
+>>>>>>> parent of 9bc59e2... Add click event to data maps
